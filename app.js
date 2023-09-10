bc38dcff5d3e2b3300adb7c48b67b64f3ba1c41a
DeepiArun <deepikaunkumarr@gmail.com>
2023-09-10 10:32:00 +0300

    Changed response

--- app.js
+++ app.js
@@ -4,10 +4,4 @@ const handleRequest = (request) => {
   return new Response("Hello world!");
 };
 
-serve(handleRequest, { port: 7777 });import { serve } from "./deps.js";
-
-const handleRequest = (request) => {
-  return new Response("Hello world and HI!");
-};
-
-serve(handleRequest, { port: 7777 });
+serve (handleRequest, { port: 7777 });
